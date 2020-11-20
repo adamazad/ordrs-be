63f25591714a122abcfdac5a0349e1d8c485e4f6
Adam Azad <adam@adamazad.com>
2020-11-20 11:30:13 +0300

    Init config

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig');
+
+module.exports = {
+  roots: ['<rootDir>'],
+  testEnvironment: 'node',
+  coveragePathIgnorePatterns: ['/node_modules/', '/build/'],
+  testTimeout: 30000,
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/src',
+  }),
+};
